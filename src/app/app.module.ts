bb904380fd639fafeeff3f391e7ad53d6891aab4
igoradigey01 <ett66@mail.ru>
2022-10-06 06:18:06 +0300

    feat(ng*14.2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -28,7 +28,7 @@ import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
+  //  BrowserAnimationsModule,
     RouterModule,
     AppRoutingModule,
   //  MaterialModule,
