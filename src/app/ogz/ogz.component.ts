c522a9f12a181f7d4da57461928053a74195290f
igoradigey01 <ett66@mail.ru>
2022-10-13 17:22:20 +0300

    chore(ogz)

--- src/app/ogz/ogz.component.ts
+++ src/app/ogz/ogz.component.ts
@@ -1,77 +1,124 @@
-import { Component, OnInit ,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 export interface Point {
-  x: string;
-  y: string;
+  x: number;
+  y: number;
 }
 export interface PointDTO {
   x_firs: string;
-  x_last:string;
+  x_last: string;
   y_firs: string;
-  y_last:string;
+  y_last: string;
 }
 export enum StateView {
-  default=0,
-  edit_op=1,  
-  cell=2,
-  show_rezilt=3
-
+  default = 0,
+  edit_op = 1,
+  cell = 2,
+  show_rezilt = 3,
 }
-  
 
 @Component({
   selector: 'app-ogz',
   templateUrl: './ogz.component.html',
-  styleUrls: ['./ogz.component.scss']
+  styleUrls: ['./ogz.component.scss'],
 })
 export class OgzComponent implements OnInit {
+  @ViewChild('myCellForm', { static: false }) myCellForm: NgForm | undefined;
+  @ViewChild('myOpForm', { static: false }) myOpForm: NgForm | undefined;
+  _flag_sendData: boolean = false;
+  _flagViewState: StateView = StateView.default;
 
-  @ViewChild('myCellForm', {static: false}) myCellForm: NgForm|undefined;
-  _flag_sendData:boolean=false;
-  _flagViewState:StateView=StateView.default;
+  _op: PointDTO = <PointDTO>{};
 
-  _op:PointDTO=<PointDTO>{}
+  _cell: PointDTO = <PointDTO>{};
+
+  dir_ygol:number=0
 
   //-------------------
-  _deltaPabInput:any;
-  _threeRowInput:any;
-  selectedStatus:string ='-'; 
 
-  _dirYgol:string='';
+  public set Op_Point(a: PointDTO) {
+    localStorage.setItem('op', JSON.stringify(a));
+  }
+  public get Op_Point(): PointDTO {
+    let op = localStorage.getItem('op');
+    if (op != null) return JSON.parse(op);
+    return <PointDTO>{};
+  }
 
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this._op = this.Op_Point;
   }
 
-  editOP(){
-    this._flagViewState=StateView.edit_op;
-  }
-  calculeteData(){
-    this._flagViewState=StateView.show_rezilt;
+  calculeteData() {
+       debugger
+   let op=this.getPoint(this.Op_Point);
+   let cel= this.getPoint(this._cell);
+   
+  let xDelta = cel.x-op.x;
+  let yDelta = cel.y -op.y;
+         
+  let rN=Math.abs(yDelta)/Math.abs(xDelta);
 
-  }  
+  let rumb=Math.atan(rN);
 
-  saveOP(){
-    this._flagViewState=StateView.cell;
+  if(xDelta<0&&yDelta<0){
+     this.dir_ygol=180+rumb;
 
-  }
-
-  undo(){
-    this._flagViewState=StateView.cell;
-
-  }
-
-  clear(){
-  //  this._threeRowInput=[<PabInputDto>{},<PabInputDto>{},<PabInputDto>{}]
-  if(this.myCellForm){
-    this.myCellForm.resetForm()
-  }
+    this._flagViewState = StateView.show_rezilt;
+    return
   }
 
-  private set A_Point(a:Point){}
-  
+  if(xDelta<0&&yDelta>0){
+    this.dir_ygol=180-rumb;
 
+   this._flagViewState = StateView.show_rezilt;
+   return
+ }
+
+ if(xDelta>0&&yDelta<0){
+  this.dir_ygol=360-rumb;
+
+ this._flagViewState = StateView.show_rezilt;
+ return
+}
+
+
+
+    
+  }
+
+  private getPoint(d:PointDTO):Point{
+  let x1 = + `${d.x_firs}${d.x_last}`
+  let y1 = + `${d.y_firs}${d.y_last}`
+    return <Point>{x:x1,y:y1};
+  }
+
+  saveOP() {
+    this.Op_Point = this._op;
+    this._flagViewState = StateView.cell;
+  }
+
+  undo() {
+    this._flagViewState = StateView.cell;
+  }
+  goOP(){
+    this._flagViewState = StateView.default;
+
+  }
+  clearOP() {
+    if (this.myOpForm) {
+      this.myOpForm.resetForm();
+      this.Op_Point=<PointDTO>{}
+    }
+  }
+
+  clearCell() {
+    //  this._threeRowInput=[<PabInputDto>{},<PabInputDto>{},<PabInputDto>{}]
+    if (this.myCellForm) {
+      this.myCellForm.resetForm();
+    }
+  }
 }
